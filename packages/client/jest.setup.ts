5fb6da651f0e16a1d916f79af83b4b89a3cebfe4
LapEv <e_lap@mail.ru>
2023-10-04 17:05:20 +0300

    fix jest errors

--- packages/client/jest.setup.ts
+++ packages/client/jest.setup.ts
@@ -1,2 +1,10 @@
 import '@testing-library/jest-dom'
 import '@testing-library/react'
+
+global.__reanimatedWorkletInit = () => {}
+
+jest.mock('react-native-reanimated', () => {
+  const Reanimated = require('react-native-reanimated/mock')
+  Reanimated.default.call = () => {}
+  return Reanimated
+})

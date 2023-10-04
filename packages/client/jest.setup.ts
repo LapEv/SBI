84d1b2a3025ea9afba3a0a5b7951032daa99aed0
LapEv <e_lap@mail.ru>
2023-10-04 17:05:34 +0300

    fix jest errors

--- packages/client/jest.setup.ts
+++ packages/client/jest.setup.ts
@@ -1,10 +1,12 @@
 import '@testing-library/jest-dom'
 import '@testing-library/react'
 
+// eslint-disable-next-line @typescript-eslint/no-empty-function
 global.__reanimatedWorkletInit = () => {}
 
 jest.mock('react-native-reanimated', () => {
   const Reanimated = require('react-native-reanimated/mock')
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   Reanimated.default.call = () => {}
   return Reanimated
 })

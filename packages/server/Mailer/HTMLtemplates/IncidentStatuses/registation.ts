7346695ebbc8daa1941c760fbb75c49376db8540
Evgeniy <e_lap@mail.ru>
2024-02-15 11:16:44 +0300

    add mailer to incident function

--- packages/server/Mailer/HTMLtemplates/IncidentStatuses/registation.ts
+++ packages/server/Mailer/HTMLtemplates/IncidentStatuses/registation.ts
@@ -26,117 +26,49 @@ export const htmlRegistration = ({
       <base target="_top">
     </head>
     <body>
-    <div
-    style="width: 500px; height: auto;padding: 30px; font-size: 16px; font-weight: 100;color: #000000;">
-    <div
-      style="width: 100%;height: auto;display: flex;justify-content: flex-start;align-items: flex-start;font-size: 16px;font-weight: 700;font-family: 'Calibri';color: #000000;margin-left: 10px">
-      ${
-        mailConst.mailMessages.Incidents.addRequest
-      }<p style="width: 75%; margin-left: 10px; font-size: 14px;">${incident}</p>
-    </div>
-    <div
-    style="padding: 2px; padding-left: 0px; width: 100%;height: auto;display: flex;justify-content: flex-start;align-items: flex-start;font-size: 12px;font-weight: 100;font-family: 'Calibri';color: #000000;margin-left: 10px">
-      ${
-        mailConst.mailMessages.Incidents.status
-      }<p style=" width: 80%; margin: 0;margin-left: 10px; font-size: 13px;">${status}</p>
-    </div>
-    <div
-    style="padding: 2px; padding-left: 0px; width: 100%;height: auto;display: flex;justify-content: flex-start;align-items: flex-start;font-size: 12px;font-weight: 100;font-family: 'Calibri';color: #000000;margin-left: 10px">
-      ${
-        mailConst.mailMessages.Incidents.client
-      }<p style="width: 80%; margin: 0;margin-left: 10px; font-size: 13px;">${client}</p>
-    </div>
-    ${
-      clientINC
-        ? `<div
-    style="padding: 2px; padding-left: 0px; width: 100%;height: auto;display: flex;justify-content: flex-start;align-items: flex-start;font-size: 12px;font-weight: 100;font-family: 'Calibri';color: #000000;margin-left: 10px">
-      ${mailConst.mailMessages.Incidents.clientINC}<p style="width: 80%; margin: 0;margin-left: 10px; font-size: 13px;">${clientINC}</p>
-    </div>`
-        : ''
-    }
-    <div
-    style="padding: 2px; padding-left: 0px; width: 100%;height: auto;display: flex;justify-content: flex-start;align-items: flex-start;font-size: 12px;font-weight: 100;font-family: 'Calibri';color: #000000;margin-left: 10px">
-      ${
-        mailConst.mailMessages.Incidents.timeRegistration
-      }<p style="width: 80%; margin: 0;margin-left: 10px; font-size: 13px;">${timeRegistration}</p>
-    </div>
-    <div
-    style="padding: 2px; padding-left: 0px; width: 100%;height: auto;display: flex;justify-content: flex-start;align-items: flex-start;font-size: 12px;font-weight: 100;font-family: 'Calibri';color: #000000;margin-left: 10px">
-      ${
-        mailConst.mailMessages.Incidents.timeSLA
-      }<p style="width: 80%; margin: 0;margin-left: 10px; font-size: 13px;">${timeSLA}</p>
-    </div>
-    <div
-    style="padding: 2px; padding-left: 0px; width: 100%;height: auto;display: flex;justify-content: flex-start;align-items: flex-start;font-size: 12px;font-weight: 100;font-family: 'Calibri';color: #000000;margin-left: 10px">
-      ${
-        mailConst.mailMessages.Incidents.object
-      }<p style="width: 80%; margin: 0;margin-left: 10px; font-size: 13px;">${object}</p>
-    </div>
-    ${
-      objectClientID
-        ? `<div
-    style="padding: 2px; padding-left: 0px; width: 100%;height: auto;display: flex;justify-content: flex-start;align-items: flex-start;font-size: 12px;font-weight: 100;font-family: 'Calibri';color: #000000;margin-left: 10px">
-      ${mailConst.mailMessages.Incidents.objectClientID}<p style="width: 80%; margin: 0;margin-left: 10px; font-size: 13px;">${objectClientID}</p>
-    </div>`
-        : ''
-    }
-    ${
-      objectClientName
-        ? `<div
-    style="padding: 2px; padding-left: 0px; width: 100%;height: auto;display: flex;justify-content: flex-start;align-items: flex-start;font-size: 12px;font-weight: 100;font-family: 'Calibri';color: #000000;margin-left: 10px">
-      ${mailConst.mailMessages.Incidents.objectClientName}<p style="width: 80%; margin: 0;margin-left: 10px; font-size: 13px;">${objectClientName}</p>
-    </div>`
-        : ''
-    }
-    <div
-    style="padding: 2px; padding-left: 0px; width: 100%;height: auto;display: flex;justify-content: flex-start;align-items: flex-start;font-size: 12px;font-weight: 100;font-family: 'Calibri';color: #000000;margin-left: 10px">
-      ${
-        mailConst.mailMessages.Incidents.address
-      }<p style="width: 80%; margin: 0;margin-left: 10px; font-size: 13px;">${address}</p>
-    </div>
-    <div
-    style="padding: 2px; padding-left: 0px; width: 100%;height: auto;display: flex;justify-content: flex-start;align-items: flex-start;font-size: 12px;font-weight: 100;font-family: 'Calibri';color: #000000;margin-left: 10px">
-      ${
-        mailConst.mailMessages.Incidents.equipment
-      }<p style="width: 80%; margin: 0;margin-left: 10px; font-size: 13px;">${equipment}</p>
-    </div>
-    <div
-    style="padding: 2px; padding-left: 0px; width: 100%;height: auto;display: flex;justify-content: flex-start;align-items: flex-start;font-size: 12px;font-weight: 100;font-family: 'Calibri';color: #000000;margin-left: 10px">
-      ${
-        mailConst.mailMessages.Incidents.model
-      }<p style="width: 80%; margin: 0;margin-left: 10px; font-size: 13px;">${model}</p>
-    </div>
-    <div
-    style="padding: 2px; padding-left: 0px; width: 100%;height: auto;display: flex;justify-content: flex-start;align-items: flex-start;font-size: 12px;font-weight: 100;font-family: 'Calibri';color: #000000;margin-left: 10px">
-      ${
-        mailConst.mailMessages.Incidents.malfunction
-      }<p style="width: 80%; margin: 0;margin-left: 10px; font-size: 13px;">${malfunction}</p>
-    </div>
+    <table
+    style="width: auto; height: auto; padding: 10px; font-size: 11px; font-weight: 100;font-family: 'Calibri', sans-serif;color: #000000;">
+    <tr style="font-size: 14px; font-weight: 700;">
+      <td style="width: 40%">
+        ${mailConst.mailMessages.Incidents.addRequest}
+      </td>
+      <td>${incident}</td>
+    </tr>
+    <table style="width: auto; font-size: 12px; font-weight: 100; margin-top: 10px;  height: auto; padding: 10px;">
+      <tr>
+        <td style="font-weight: 700; vertical-align: top">
+        ${mailConst.mailMessages.Incidents.status}
+        </td>
+        <td>${status}</td>
+      </tr>
+      <tr>
+        <td style="font-weight: 700; vertical-align: top">
+        ${mailConst.mailMessages.Incidents.client}
+        </td>
+        <td style="margin-left: 10px">${client}</td>
+      </tr>
+      {${clientINC} && 
+        <tr>
+          <td style="font-weight: 700; vertical-align: top">
+          ${mailConst.mailMessages.Incidents.clientINC}
+          </td>
+          <td style="margin-left: 10px">${clientINC}</td>
+        </tr>
+      }
+      <tr>
+        <td style="font-weight: 700; vertical-align: top">
+          Описание:
+        </td>
+        <td style="margin-left: 10px">Плохо пропечатывается этикткаПлохо пропечатывается этикткаПлохо пропечатывается
+          этикткаПлохо пропечатывается этикткаПлохо пропечатывается этикткаПлохо пропечатывается этикткаПлохо
+          пропечатывается этикткаПлохо пропечатывается этикткаПлохо пропечатывается этикткаПлохо пропечатывается
+          этикткаПлохо пропечатывается этикткаПлохо пропечатывается этикткаПлохо пропечатывается этикткаПлохо
+          пропечатывается этикткаПлохо пропечатывается этикткаПлохо пропечатывается этикткаПлохо пропечатывается
+          этикткаПлохо пропечатывается этиктка</td>
+      </tr>
 
-    <div
-    style="padding: 2px; padding-left: 0px; width: 100%;height: auto;display: flex;justify-content: flex-start;align-items: flex-start;font-size: 12px;font-weight: 100;font-family: 'Calibri';color: #000000;margin-left: 10px">
-      ${
-        mailConst.mailMessages.Incidents.description
-      }<p style="width: 80%; margin: 0;margin-left: 10px; font-size: 13px;">${description}</p>
-    </div>
-    <div
-    style="padding: 2px; padding-left: 0px; width: 100%;height: auto;display: flex;justify-content: flex-start;align-items: flex-start;font-size: 12px;font-weight: 100;font-family: 'Calibri';color: #000000;margin-left: 10px">
-      ${
-        mailConst.mailMessages.Incidents.applicant
-      }<p style="width: 80%; margin: 0;margin-left: 10px; font-size: 13px;">${applicant}</p>
-    </div>
-    <div
-    style="padding: 2px; padding-left: 0px; width: 100%;height: auto;display: flex;justify-content: flex-start;align-items: flex-start;font-size: 12px;font-weight: 100;font-family: 'Calibri';color: #000000;margin-left: 10px">
-      ${
-        mailConst.mailMessages.Incidents.applicantContacts
-      }<p style="width: 80%; margin: 0;margin-left: 10px; font-size: 13px;">${applicantContacts}</p>
-    </div>
-    <div
-    style="padding: 2px; padding-left: 0px; width: 100%;height: auto;display: flex;justify-content: flex-start;align-items: flex-start;font-size: 12px;font-weight: 100;font-family: 'Calibri';color: #000000;margin-left: 10px">
-      ${
-        mailConst.mailMessages.Incidents.userAccepted
-      }<p style="width: 80%; margin: 0;margin-left: 10px; font-size: 13px;">${userAccepted}</p>
-    </div>
+    </table>
+  </table>
   <div
       style="width: 80%; margin-top: 30px; width: 100%;height: auto;display: flex;justify-content: flex-start;align-items: flex-start;font-size: 8px;font-weight: 100;font-family: 'Calibri';color: #000000;margin-left: 10px">
       ${mailConst.mailMessages.footer}

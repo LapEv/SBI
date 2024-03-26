4d4b81b860ab340be7d29753f4c2d3b6a37b73b4
Evgeniy <51084295+LapEv@users.noreply.github.com>
2024-03-26 22:52:46 +0300

    docker config change

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -15,7 +15,10 @@ export default defineConfig({
     port: Number(process.env.CLIENT_PORT) || 3000,
   },
   define: {
-    __SERVER_HOST__: JSON.stringify(process.env.SERVER_HOST),
+    __SERVER_HOST__:
+      process.env.NODE_ENV === 'development'
+        ? JSON.stringify(process.env.SERVER_HOST)
+        : JSON.stringify(process.env.SERVER_DOCKER_HOST),
     __SERVER_PORT__: process.env.SERVER_PORT,
   },
   plugins: [react()],

ed0fc315751c76d59ff4c5efd2164a200ecda9d8
Evgeniy <51084295+LapEv@users.noreply.github.com>
2024-04-01 19:34:25 +0300

    fix path ssr errors

--- packages/server/data/app.ts
+++ packages/server/data/app.ts
@@ -2,9 +2,9 @@ import path from 'path'
 
 export const appConstants = { appName: 'SBI_DB' }
 export const isDev = process.env.NODE_ENV === 'development'
-export const srcPath = path.resolve(__dirname, '../../../client')
-export const distPath = path.resolve(__dirname, '../../../client/dist/')
+export const srcPath = path.resolve(__dirname, '../../client/')
+export const distPath = path.resolve(__dirname, '../../client/dist/')
 export const ssrClientPath = path.resolve(
   __dirname,
-  '../../../client/dist-ssr/client.cjs'
+  '../../client/dist-ssr/client.cjs'
 )
